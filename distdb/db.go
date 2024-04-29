ace9b714d27d5f782c01c9dcab715e9844824e88
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-04-30 01:56:50 +0530

    feat: add concurrency to server using socket

--- distdb/db.go
+++ distdb/db.go
@@ -5,14 +5,34 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"net"
 	"os"
+	"sync"
 )
 
 var ErrKeyDoesNotExist = errors.New("this key does not exist")
+var ErrInvalidOperation = errors.New("invalid operation")
 
 const (
-	DISKFILENAME = "dbdump"
+	DISKFILENAME    = "dbdump"
+	SERVER_PROTOCOL = "tcp"
+	SERVER_HOST     = "localhost"
+	SERVER_PORT     = "3108"
+	SUCCESS         = "SUCCESS"
+	FAILURE         = "FAILURE"
+	GET             = "GET"
+	PUT             = "PUT"
 )
+
+type Request struct {
+	Key, Val []byte
+	Op       string
+}
+
+type Response struct {
+	Status, Error string
+	Val           []byte
+}
 
 type DBEntry struct {
 	Key, Val []byte
@@ -21,10 +41,26 @@ type DBEntry struct {
 type DB struct {
 	Entries []*DBEntry
 	f       *os.File
+	mu      *sync.Mutex
+	config  DBConfig
 }
 
-func NewDB() (*DB, error) {
-	/* Open file, get data and keep it in memory */
+type DBConfig struct {
+	Persist bool
+}
+
+func NewDBConfig(persist bool) DBConfig {
+	return DBConfig{Persist: persist}
+}
+
+func NewDB(config DBConfig) (*DB, error) {
+	/* If db is not persistant */
+	db := &DB{Entries: []*DBEntry{}, mu: &sync.Mutex{}, config: config}
+	if !config.Persist {
+		return db, nil
+	}
+
+	/* If persistant open file, get data and keep it in memory */
 	f, err := os.OpenFile(DISKFILENAME, os.O_RDWR|os.O_CREATE, 0777)
 	if err != nil {
 		return nil, err
@@ -39,11 +75,85 @@ func NewDB() (*DB, error) {
 		}
 	}
 
-	return &DB{f: f, Entries: entries}, nil
+	db.f = f
+	return db, nil
 }
 
 func newDBEntry(key, val []byte) DBEntry {
 	return DBEntry{Key: key, Val: val}
+}
+
+func (db *DB) Listen() error {
+	server, err := net.Listen(SERVER_PROTOCOL, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		return err
+	}
+	defer server.Close()
+
+	for {
+		clientConn, err := server.Accept()
+		if err != nil {
+			return err
+		}
+
+		go db.handleConn(clientConn)
+	}
+}
+
+func (db *DB) handleConn(conn net.Conn) error {
+	buffer := make([]byte, 1024)
+
+	/* Read client request */
+	clientMessageLen, err := conn.Read(buffer)
+	if err != nil {
+		return err
+	}
+
+	/* Unmarshal request sent as JSON */
+	var clientRequest Request
+	clientMessage := buffer[:clientMessageLen]
+	err = json.Unmarshal(clientMessage, &clientRequest)
+	if err != nil {
+		return err
+	}
+
+	/* Formulate response according to the operation requested */
+	var resp Response
+	switch clientRequest.Op {
+	case GET:
+		val, err := db.Get(clientRequest.Key)
+		if err != nil {
+			resp.Error = err.Error()
+			resp.Status = FAILURE
+			break
+		}
+		resp.Val = val
+		resp.Status = SUCCESS
+	case PUT:
+		err := db.Put(clientRequest.Key, clientRequest.Val)
+		if err != nil {
+			resp.Error = err.Error()
+			resp.Status = FAILURE
+			break
+		}
+		resp.Status = SUCCESS
+	default:
+		resp.Error = ErrInvalidOperation.Error()
+		resp.Status = FAILURE
+	}
+
+	/* Marshal response to JSON */
+	respData, err := json.Marshal(resp)
+	if err != nil {
+		return err
+	}
+
+	/* Send response */
+	if _, err = conn.Write(respData); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (db *DB) Get(key []byte) (val []byte, err error) {
@@ -67,19 +177,28 @@ func (db *DB) get(key []byte) (entry *DBEntry, err error) {
 
 func (db *DB) Put(key, val []byte) error {
 	entry, err := db.get(key)
+	db.mu.Lock()
+	defer db.mu.Unlock()
 	if err != nil {
 		if errors.Is(err, ErrKeyDoesNotExist) {
 			newEntry := newDBEntry(key, val)
 			db.Entries = append(db.Entries, &newEntry)
+			if !db.config.Persist {
+				return nil
+			}
 			return db.writeToDisk()
 		}
 		return err
 	}
 
 	entry.Val = val
+	if !db.config.Persist {
+		return nil
+	}
 	return db.writeToDisk()
 }
 
+/* Call this only with db.Mutex held */
 func (db *DB) writeToDisk() error {
 	/* Truncate entire file */
 	_, err := db.f.Seek(0, 0)
--- distdb/db_test.go
+++ distdb/db_test.go
@@ -1,13 +1,18 @@
 package distdb
 
 import (
+	"encoding/json"
+	"sync"
 	"testing"
 
+	"github.com/chettriyuvraj/distributed-kv-store/client"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetPut(t *testing.T) {
-	db, err := NewDB()
+	config := NewDBConfig(false)
+
+	db, err := NewDB(config)
 	require.NoError(t, err)
 
 	/* Get a non existing key */
@@ -30,4 +35,52 @@ func TestGetPut(t *testing.T) {
 	v2FromDB, err := db.Get(k1)
 	require.NoError(t, err)
 	require.Equal(t, v2, v2FromDB)
+}
+
+/* TODO: test mutex? currently not tested, also how would you test it? */
+func TestHandleConn(t *testing.T) {
+	config := NewDBConfig(false)
+
+	/* Create server and start listening */
+	db, err := NewDB(config)
+	require.NoError(t, err)
+
+	go db.Listen()
+
+	/* Start a new goroutine, with a new client for each request - TODO: error cases */
+	tcs := []struct {
+		req      Request
+		respWant Response
+	}{
+		{req: Request{Key: []byte("key2"), Val: []byte("val2"), Op: PUT}, respWant: Response{Status: SUCCESS, Error: "", Val: nil}},
+		{req: Request{Key: []byte("key2"), Op: GET}, respWant: Response{Status: SUCCESS, Error: "", Val: []byte("val2")}},
+		{req: Request{Key: []byte("key1"), Op: GET}, respWant: Response{Status: FAILURE, Error: ErrKeyDoesNotExist.Error(), Val: nil}},
+	}
+
+	/* Wait Group to ensure test completes only when each request finishes */
+	var wg sync.WaitGroup
+
+	for _, tc := range tcs {
+		wg.Add(1)
+
+		client, err := client.NewClient()
+		require.NoError(t, err)
+
+		req := tc.req
+
+		go func(respWant Response) {
+			defer wg.Done()
+			client.MakeRequest(req.Key, req.Val, req.Op)
+			require.NoError(t, err)
+
+			var response Response
+			respData, err := client.RcvResponse()
+			require.NoError(t, err)
+			err = json.Unmarshal(respData, &response)
+			require.NoError(t, err)
+			require.Equal(t, respWant, response)
+		}(tc.respWant)
+	}
+
+	wg.Wait()
 }
